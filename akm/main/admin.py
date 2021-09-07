97b024ab7301474c292d8a9017eda5a991d95ba5
kliutko <kliutko.fr@ya.ru>
2021-09-07 22:42:48 +0300

    finished header and footer, the specialty block has been moved to a separate application

--- akm/main/admin.py
+++ akm/main/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Feedback)
-admin.site.register(News)
+
+
+admin.site.register(SettingsHeader)
+
